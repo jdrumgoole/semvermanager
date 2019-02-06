b4806aab504bff590ba5ed666822468b72b5b5b7
jdrumgoole <joe@joedrumgoole.com>
2019-02-06 23:54:45 +0000

    Checkin for release to pypi

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Joe Drumgoole'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.2-alpha10'
+release = '0.0.2-alpha11'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/jdrumgoole/semvermanager'
 EMAIL = 'joe@joedrumgoole.com'
 AUTHOR = 'Joe Drumgoole'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.4-alpha10'
+VERSION = '0.0.4-alpha11'
 
 # What packages are required for this module to be executed?
 REQUIRED = []
